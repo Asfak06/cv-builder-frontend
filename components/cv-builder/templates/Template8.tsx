1a67cc497b9cfa01452efac3ea4fe6e721d484c1
MilonSarker40 <43253773+MilonSarker40@users.noreply.github.com>
2025-07-14 18:02:32 +0600

    cv template 7 done

--- components/cv-builder/templates/Template8.tsx
+++ components/cv-builder/templates/Template8.tsx
@@ -3,18 +3,15 @@ import parse from "html-react-parser";
 import { Inter } from "next/font/google";
 import {
     FaCode,
-    FaEnvelope,
     FaFacebookF,
     FaGithub,
     FaInstagramSquare,
     FaLinkedinIn,
-    FaMapMarkerAlt,
     FaMobileAlt,
-    FaPhone,
     FaRegLightbulb,
     FaTools,
     FaTwitter,
-    FaYoutube,
+    FaYoutube
 } from "react-icons/fa";
 import { IoIosSearch } from "react-icons/io";
 import { TbWorld } from "react-icons/tb";
@@ -40,7 +37,7 @@ export default function Template2() {
     } = useCVStore();
 
     const skillIconMap: Record<string, React.ElementType> = {
-        product: FaRegLightbulb,
+        ProductDesign: FaRegLightbulb,
         research: IoIosSearch,
         uiux: FaMobileAlt,
         code: FaCode,
@@ -51,7 +48,7 @@ export default function Template2() {
             className={`bg-[#fff] max-w-[1000px] mx-auto border-0 min-h-[1123px] ${inter.className}`}
         >
             <div className="flex justify-left items-center px-8">
-                <div className="flex justify-left items-center border-b-[1px] border-[#E8E8E8] py-8">
+                <div className="flex justify-left items-center w-full border-b-[1px] border-[#E8E8E8] py-8">
                     {/* Header Section */}
                     <div className="flex justify-center pr-[30px]">
                         <img
@@ -75,66 +72,34 @@ export default function Template2() {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col md:flex-row  px-[20px] py-[30px]">
+            <div className="flex flex-col md:flex-row  px-[30px] py-[30px]">
                 {/* Left Column */}
-                <div className="text-white border-r-2 border-[#f4f4f4] pr-3 w-[35%]">
+                <div className="text-white pr-6 w-[35%]">
                     {/* Skills */}
-                    {skills.length > 0 && (
-                        <section>
-                            <h2 className="section-heading">Skills</h2>
-                            <ul className="mt-3 space-y-1">
+                    <div className="mb-10">
+                        <h2 className="text-[30px] text-[#141414] capitalize font-medium pb-1">Skills</h2>
+                        {skills.length > 0 && (
+                            <ul className="mt-3 space-y-3">
                                 {skills.map((skill, idx) => {
                                     const Icon = skillIconMap[skill.toLowerCase()] || FaTools;
                                     return (
-                                        <li key={idx} className="flex items-center gap-2 text-[#707070]">
-                                            <Icon className="text-[#FFCB14]" size={16} />
+                                        <li key={idx} className="flex items-center gap-2 bg-[#FFFFFF] p-[10px] border border-[#F4F4F4] shadow-[0_1.68px_6.74px_rgba(20,20,43,0.05)] text-lg text-[#141414]">
+                                            <span className="bg-[#E8E8E8] inline-block p-2 rounded-[5px]"><Icon className=" text-[#5B5B5B]" size={16} /></span>
                                             {skill}
                                         </li>
                                     );
                                 })}
                             </ul>
-                        </section>
-                    )}
-                    {/* Contact Information */}
-                    <div className="">
-                        <h2 className="text-[26px] text-[#040404] uppercase font-bold pb-1 pt-3 relative before:content-[''] before:w-[12px] before:h-[2px] before:bg-[#FFCB14] before:absolute before:left-[0] before:top-[6px]">
-                            Contact
-                        </h2>
-                        <p className="mt-2 pb-1">
-                            {" "}
-                            <strong className="flex justify-start items-center text-[16px] text-[#707070] uppercase">
-                                <FaEnvelope className="mr-2" /> Email
-                            </strong>{" "}
-                            <span className="block text-[16px] pl-[18px] text-[#040404] leading-none">
-                                {personalDetails.email}
-                            </span>
-                        </p>
-                        <p className="mt-2 pb-1">
-                            <strong className="flex justify-start items-center text-[16px] text-[#707070] uppercase">
-                                <FaPhone className="mr-2" /> Phone
-                            </strong>{" "}
-                            <span className="block text-[16px] pl-[18px] text-[#040404] leading-none">
-                                {personalDetails.phone}
-                            </span>
-                        </p>
-                        <p className="mt-2 pb-1">
-                            <strong className="flex justify-start items-center text-[16px] text-[#707070] uppercase">
-                                <FaMapMarkerAlt className="mr-2" /> Address
-                            </strong>{" "}
-                            <span className="block text-[16px] pl-[18px] text-[#040404] leading-none">
-                                {personalDetails.city}, {personalDetails.country}
-                            </span>
-                        </p>
+                        )}
                     </div>
-
                     {/* Links Section */}
                     {links.length > 0 && (
-                        <div className="mt-8 space-y-2 pb-4">
-                            <h2 className="text-[26px] text-[#040404] uppercase font-bold pb-1 pt-3 relative before:content-[''] before:w-[12px] before:h-[2px] before:bg-[#FFCB14] before:absolute before:left-[0] before:top-[6px]">
+                        <div className="mt-8 space-y-3 pb-4">
+                            <h2 className="text-[30px] text-[#141414] capitalize font-medium">
                                 Link
                             </h2>
-                            <p className="text-[16px] text-[#707070] font-bold pb-1">@johncarter</p>
-                            <div className="flex justify-start items-center">
+                            <p className="text-[16px] text-[#666666] mt-0 pb-3">@peterdesigner</p>
+                            <div className="">
                                 {links.map((link, index) => {
                                     let Icon = TbWorld; // Default icon
 
@@ -162,14 +127,15 @@ export default function Template2() {
                                     }
 
                                     return (
-                                        <p key={index} className="flex items-center">
+                                        <p key={index} className="flex justify-center items-center mb-3 w-full bg-[#FFFFFF] p-[10px] border border-[#F4F4F4] shadow-[0_1.68px_6.74px_rgba(20,20,43,0.05)] text-lg font-medium text-[#141414]">
                                             <a
                                                 href={link.url}
                                                 target="_blank"
                                                 rel="noopener noreferrer"
-                                                className="text-[#fff] text-[16px] pb-1 hover:underline"
+                                                className="flex justify-center items-center hover:underline"
                                             >
-                                                <Icon className="mr-3 text-[#040404]" />
+                                                <Icon className="mr-2 text-[#040404]" size={20} />
+                                                <span className="text-[#141414]">{link.label}</span>
                                             </a>
                                         </p>
                                     );
@@ -177,11 +143,10 @@ export default function Template2() {
                             </div>
                         </div>
                     )}
-
                     {/* Hobbies Section */}
                     {hobbies.length > 0 && (
                         <div className="mt-4">
-                            <h2 className="text-[26px] text-[#040404] uppercase font-bold pb-1 pt-3 relative before:content-[''] before:w-[12px] before:h-[2px] before:bg-[#FFCB14] before:absolute before:left-[0] before:top-[6px]">
+                            <h2 className="text-[30px] text-[#141414] capitalize font-medium">
                                 Hobbies
                             </h2>
                             <ul className="text-gray-300 text-sm mt-2 space-y-1">
@@ -199,16 +164,16 @@ export default function Template2() {
 
                     {/* References */}
                     <div className="mt-5">
-                        <h2 className="text-[26px] text-[#040404] uppercase font-bold pb-1 pt-3 relative before:content-[''] before:w-[12px] before:h-[2px] before:bg-[#FFCB14] before:absolute before:left-[0] before:top-[6px]">
+                        <h2 className="text-[30px] text-[#141414] capitalize font-medium">
                             References
                         </h2>
                         {references.map((ref, index) => (
-                            <p key={index} className="text-gray-700 text-[16px] mt-2">
-                                <strong className="text-[10px]">{ref.name}</strong>{" "}
-                                <span className="flex items-center mt-1 text-gray-700 pb-1 text-[9px]">
+                            <p key={index} className="text-[#141414] text-[16px] mt-2">
+                                <strong className="text-[18px]">{ref.name}</strong>{" "}
+                                <span className="flex items-center mt-1 text-[#141414] pb-1 text-[16px]">
                                     {ref.position}
                                 </span>
-                                <span className="flex items-center text-gray-700 pb-1 text-[10px]">
+                                <span className="flex items-center text-[#141414] pb-1 text-[16px]">
                                     {ref.company}
                                 </span>
                             </p>
@@ -217,29 +182,29 @@ export default function Template2() {
                 </div>
 
                 {/* Right Column */}
-                <div className="pl-5 w-[65%]">
+                <div className="pl-8 w-[65%]">
                     {/* Profile Summary */}
-                    <div className="border-b-2 border-[#f4f4f4] pb-4">
-                        <h2 className="text-[26px] text-[#040404] uppercase font-bold pb-1 pt-3 relative before:content-[''] before:w-[12px] before:h-[2px] before:bg-[#FFCB14] before:absolute before:left-[0] before:top-[6px]">
+                    <div className="border-b-2 border-[#f4f4f4] pb-10">
+                        <h2 className="text-[30px] text-[#141414] capitalize font-medium">
                             About me
                         </h2>
-                        <p className="text-[#707070] text-[16px] mt-1">{parse(summary)}</p>
+                        <p className="text-[#666666] text-[16px] mt-1">{parse(summary)}</p>
                     </div>
 
                     {/* Work Experience */}
-                    <div className="mt-5 pb-2 border-b-2 border-[#f4f4f4]">
-                        <h2 className="text-[26px] text-[#040404] uppercase font-bold pb-1 pt-3 relative before:content-[''] before:w-[12px] before:h-[2px] before:bg-[#FFCB14] before:absolute before:left-[0] before:top-[6px]">
+                    <div className="mt-10 pb-2">
+                        <h2 className="text-[30px] mb-2 text-[#141414] capitalize font-medium">
                             Work Experience
                         </h2>
                         {experience.map((exp, index) => (
                             <div key={index} className="mt-1 mb-4">
-                                <h3 className="text-[18px] text-[#040404] mb-2 capitalize font-medium">
+                                <h3 className="text-[18px] text-[#141414] mb-1 capitalize font-bold">
                                     {exp.jobTitle}
                                 </h3>
                                 <p className="text-[16px] text-[#707070E5] mb-2">
-                                    <strong className="text-[16px] text-[#030B1A] font-bold">
+                                    <strong className="text-[16px] text-[#666666] font-normal mb-1 block">
                                         {exp.company}
-                                    </strong>{" "}- {exp.startDate} / {exp.endDate}
+                                    </strong> {exp.startDate} - {exp.endDate}
                                 </p>
                                 <p className="text-[#707070] text-[16px] mt-1">
                                     {parse(exp.description)}
@@ -250,18 +215,22 @@ export default function Template2() {
 
                     {/* Education */}
                     <div className="mt-5">
-                        <h2 className="text-[26px] text-[#040404] uppercase font-bold pb-1 pt-3 relative before:content-[''] before:w-[12px] before:h-[2px] before:bg-[#FFCB14] before:absolute before:left-[0] before:top-[6px]">
+                        <h2 className="text-[30px] mb-2 text-[#141414] capitalize font-medium">
                             Education
                         </h2>
                         <div className="flex flex-wrap justify-left items-center">
                             {education.map((edu, index) => (
                                 <div key={index} className="mt-2 pr-2 w-full">
-                                    <p className="text-[#707070] text-[16px] font-medium">
-                                        {edu.institution} - {edu.year}
-                                    </p>
-                                    <h3 className="text-[16px] text-[#040404] mb-2 font-bold">
+                                    <h3 className="text-[18px] text-[#141414] mb-1 capitalize font-bold">
                                         {edu.degree}
                                     </h3>
+                                    <p className="text-[16px] text-[#707070E5]">
+                                        <p>{edu.institution}</p>
+                                        {edu.year}
+                                    </p>
+                                    <p className="text-[#707070] text-[16px] mt-1">
+                                        {parse(edu.description)}
+                                    </p>
                                 </div>
                             ))}
                         </div>
@@ -305,6 +274,41 @@ export default function Template2() {
                                 ))}
                             </div>
                         ))}
+                    </div>
+                </div>
+            </div>
+            <div className="p-[30px]">
+                {/* Contact Information */}
+                <div className="border-t border-[#E8E8E8] pt-10">
+                    <h2 className="text-[30px] text-[#141414] capitalize font-medium">
+                        Contact
+                    </h2>
+                    <div className="flex justify-between items-center">
+                        <p className="mt-2 pb-1">
+                            {" "}
+                            <strong className="text-[16px] text-[#666666] font-normal capitalize mb-3">
+                                Email
+                            </strong>{" "}
+                            <span className="block text-[16px] text-[#141414] leading-none">
+                                {personalDetails.email}
+                            </span>
+                        </p>
+                        <p className="mt-2 pb-1">
+                            <strong className="text-[16px] text-[#666666] font-normal capitalize mb-3">
+                                Phone
+                            </strong>{" "}
+                            <span className="block text-[16px] text-[#141414] leading-none">
+                                {personalDetails.phone}
+                            </span>
+                        </p>
+                        <p className="mt-2 pb-1">
+                            <strong className="text-[16px] text-[#666666] font-normal capitalize mb-3">
+                                Address
+                            </strong>{" "}
+                            <span className="block text-[16px] text-[#141414] leading-none">
+                                {personalDetails.city}, {personalDetails.country}
+                            </span>
+                        </p>
                     </div>
                 </div>
             </div>
